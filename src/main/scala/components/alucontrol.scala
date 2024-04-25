57189cb04d7ebe9478c080d622d7833560601192
rameeeeenkhan <134160455+rameeeeenkhan@users.noreply.github.com>
2024-04-25 03:05:23 +0000

    Passes

--- src/main/scala/components/alucontrol.scala
+++ src/main/scala/components/alucontrol.scala
@@ -24,6 +24,7 @@ import chisel3.util._
  * This is loosely based on figure 4.12
  */
 
+
 class ALUControl extends Module {
   val io = IO(new Bundle {
     val aluop     = Input(UInt(3.W))
@@ -31,47 +32,78 @@ class ALUControl extends Module {
     val funct3    = Input(UInt(3.W))
 
     val operation = Output(UInt(5.W))
-  }) 
+  })
 
-  // Set a default operation code that indicates an invalid operation
-  io.operation := "b11111".U 
+  // Default invalid operation
+  io.operation := "b11111".U
 
-  switch(io.aluop) {
-    is("b001".U) { // Handling 64-bit R-type instructions
+//DONT USE WHENS WITHIN SWITCHES!!
+
+  when(io.aluop === "b001".U) { // For 64-bit R-type instructions
+    when(io.funct7 === "b0000001".U) { // M
+      switch(io.funct3) {
+        is("b000".U) { io.operation := "b00110".U } // MUL
+        is("b001".U) { io.operation := "b00111".U } // MULH
+        is("b010".U) { io.operation := "b11000".U } // MULHSU
+        is("b011".U) { io.operation := "b01000".U } // MULHU
+        is("b100".U) { io.operation := "b01011".U } // DIV
+        is("b101".U) { io.operation := "b01010".U } // DIVU
+        is("b110".U) { io.operation := "b11100".U } // REM
+        is("b111".U) { io.operation := "b11011".U } // REMU
+      }
+    } .otherwise { // Non-M
       switch(io.funct3) {
         is("b000".U) {
-          when(io.funct7 === "b0000001".U) { io.operation := "b00110".U } // MUL
-          .elsewhen(io.funct7 === "b0000000".U) { io.operation := "b00001".U } // ADD
-          .elsewhen(io.funct7 === "b0100000".U) { io.operation := "b00100".U } // SUB
+          when(io.funct7 === "b0100000".U) {
+            io.operation := "b00100".U // SUB
+          } .otherwise {
+            io.operation := "b00001".U // ADD
+          }
         }
-        is("b001".U) { io.operation := "b10010".U } // SLL (Shift Left Logical)
-        is("b010".U) { io.operation := "b10110".U } // SLT (Set Less Than)
-        is("b011".U) { io.operation := "b10111".U } // SLTU (Set Less Than Unsigned)
+        is("b001".U) { io.operation := "b10010".U } // SLL
+        is("b010".U) { io.operation := "b10110".U } // SLT
+        is("b011".U) { io.operation := "b10111".U } // SLTU
         is("b100".U) { io.operation := "b01111".U } // XOR
         is("b101".U) {
-          when(io.funct7 === "b0000000".U) { io.operation := "b10100".U } // SRL (Shift Right Logical)
-          .elsewhen(io.funct7 === "b0100000".U) { io.operation := "b10000".U } // SRA (Shift Right Arithmetic)
+          when(io.funct7 === "b0100000".U) {
+            io.operation := "b10000".U // SRA
+          } .otherwise {
+            io.operation := "b10100".U // SRL
+          }
         }
         is("b110".U) { io.operation := "b01110".U } // OR
         is("b111".U) { io.operation := "b01101".U } // AND
       }
     }
+  }
 
-    is("b011".U) { // Handling 32-bit R-type instructions
+  when(io.aluop === "b011".U) { // For 32-bit R-type instructions
+    when(io.funct7 === "b0000001".U) { // M
+      switch(io.funct3) {
+        is("b000".U) { io.operation := "b00101".U } // MULW
+        is("b100".U) { io.operation := "b01001".U } // DIVW
+        is("b101".U) { io.operation := "b01100".U } // DIVUW
+        is("b110".U) { io.operation := "b11010".U } // REMW
+        is("b111".U) { io.operation := "b11001".U } // REMUW
+      }
+    } .otherwise { // Non-M
       switch(io.funct3) {
         is("b000".U) {
-          when(io.funct7 === "b0000001".U) { io.operation := "b00101".U } // MULW
-          .elsewhen(io.funct7 === "b0000000".U) { io.operation := "b00000".U } // ADDW
-          .elsewhen(io.funct7 === "b0100000".U) { io.operation := "b00010".U } // SUBW
+          when(io.funct7 === "b0100000".U) {
+            io.operation := "b00010".U // SUBW
+          } .otherwise {
+            io.operation := "b00000".U // ADDW
+          }
         }
-        is("b001".U) { io.operation := "b10011".U } // SLLW (Shift Left Logical Word)
+        is("b001".U) { io.operation := "b10011".U } // SLLW
         is("b101".U) {
-          when(io.funct7 === "b0000000".U) { io.operation := "b10101".U } // SRLW (Shift Right Logical Word)
-          .elsewhen(io.funct7 === "b0100000".U) { io.operation := "b10001".U } // SRAW (Shift Right Arithmetic Word)
+          when(io.funct7 === "b0100000".U) {
+            io.operation := "b10001".U // SRAW
+          } .otherwise {
+            io.operation := "b10101".U // SRLW
+          }
         }
       }
     }
   }
 }
-
-
